688692b334652569ef62130e73ed24ac6958a231
professionalcipher <65046006+professionalcipher@users.noreply.github.com>
2020-12-11 22:39:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deploy <code>React App</code> To Zeet Server
+          Welcome To Zeet Server
         </p>
         <a
           className="App-link"
